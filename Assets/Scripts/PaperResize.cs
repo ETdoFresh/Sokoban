5f53f31cde0d6bf29f5945ac1bcc96e8b0a8e43a
Edward Garcia <etgarcia@uno.edu>
2015-11-28 23:00:56 -0600

    S02E38 - Sokoban - Inertia Scrolling and Zooming - Created a Paper Object in 3d - Created a system where Paper Resizes to Text - Created a system to drag/zoom paper around

---
+++ Assets/Scripts/PaperResize.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class PaperResize : MonoBehaviour
+{
+    Transform code;
+    Transform bottom;
+    TextMesh textMesh;
+    BoxCollider boxCollider;
+
+    // Use this for initialization
+    void Start()
+    {
+        code = transform.FindChild("Code");
+        bottom = transform.FindChild("Paper").FindChild("BottomPaper");
+        textMesh = code.GetComponent<TextMesh>();
+        boxCollider = GetComponent<BoxCollider>();
+        SetText(textMesh.text);
+    }
+    
+    public void SetText(string text)
+    {
+        textMesh.text = text;
+        int newLines = 0;
+        foreach (char c in textMesh.text) if (c == '\n') newLines++;
+        newLines -= 8;
+        newLines = Math.Max(0, newLines);
+        Vector3 scale = bottom.localScale;
+        scale.z = newLines / 57f;
+        bottom.localScale = scale;
+
+        Vector3 bcSize = boxCollider.size;
+        Vector3 bcCenter = boxCollider.center;
+        bcSize.y = newLines * 0.042f + 0.4f;
+        bcCenter.y = 2.8f - (bcSize.y / 2);
+        boxCollider.size = bcSize;
+        boxCollider.center = bcCenter;
+    }
+}
